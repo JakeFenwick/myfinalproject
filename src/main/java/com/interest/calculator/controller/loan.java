1cb84f500a79a5172b06068c1f7fd517eec64825
JakeFenwick <jakefenwick94@gmail.com>
2021-08-23 09:16:12 +1000

    Working on getting springboot working

---
+++ src/main/java/com/interest/calculator/controller/loan.java
@@ -0,0 +1,28 @@
+package com.interest.calculator.controller;
+import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class loan {
+
+    final int numOfMonths = 12;
+    final int annualInterest =;
+    final int numOfYears = ;
+
+
+    // Create a method for calculating interest/months/years
+    float monthlyInterest = annualInterest / numOfMonths;
+    int numOfPayments = numOfYears * numOfMonths;
+
+    private double principal;
+    double monthlyPayment = principal * (
+            (monthlyInterest * (Math.pow(1 + monthlyInterest, numOfPayments))) /
+                    ((Math.pow(1 + monthlyInterest, numOfPayments)) - 1)
+    );
+
+    double total = monthlyPayment / 100;
+
+}
+
