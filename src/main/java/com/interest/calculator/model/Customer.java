194b584636526ea75484a4b2569fa83a46e11f67
JakeFenwick <jakefenwick94@gmail.com>
2021-08-23 12:28:40 +1000

    Started on Customer.java building model.

--- src/main/java/com/interest/calculator/model/Customer.java
+++ src/main/java/com/interest/calculator/model/Customer.java
@@ -1,11 +1,35 @@
 package com.interest.calculator.model;
-import java.util.Scanner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 public class Customer {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    String firstName;
+    String lastName;
+    String email;
+
 
 }
 
