3636dd9a9802a43b4b27e388f1e8c53ae1ac93a6
JakeFenwick <jakefenwick94@gmail.com>
2021-08-23 13:43:51 +1000

    Repository setup

--- src/main/java/com/interest/calculator/repository/CustomerRepository.java
+++ src/main/java/com/interest/calculator/repository/CustomerRepository.java
@@ -1,7 +1,10 @@
 package com.interest.calculator.repository;
 
+import com.interest.calculator.model.Customer;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
 }
