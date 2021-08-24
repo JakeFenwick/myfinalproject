fee41c2806b5f18f4648566efeb4e0f1b912cfae
JakeFenwick <jakefenwick94@gmail.com>
2021-08-24 10:50:56 +1000

    Final README update

--- src/main/java/com/interest/calculator/controller/CustomerController.java
+++ src/main/java/com/interest/calculator/controller/CustomerController.java
@@ -1,13 +1,17 @@
 package com.interest.calculator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class CustomerController {
 
-public String intro(){
- return "/";
+ @GetMapping("/Hello")
+public String intro(Model model){
+  model.addAttribute("message","Welcome to my interest calculator");
+ return "index";
 }
 
 }
